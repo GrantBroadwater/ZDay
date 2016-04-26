d25b2fac0fd302cd94be75058e4fbc67784c69e7
Grant Broadwater <grbcp5@mst.edu>
2016-04-26 17:23:05 -0500

    Create template Neighbor struct.
    Neighbor struct represents the neighbor to a node in a graph.

---
+++ neighbor.h
@@ -0,0 +1,44 @@
+//
+// Created by Grant Broadwater on 4/26/16.
+//
+
+#ifndef ZDAY_NEIGHBOR_H
+#define ZDAY_NEIGHBOR_H
+
+template <typename T>
+struct Neighbor
+{
+  T m_id;
+  short m_cost;
+
+  Neighbor()
+      : m_id(), m_cost(0)
+  {}
+
+  Neighbor(const T& id, const short cost)
+      : m_id(id), m_cost(cost)
+  {}
+
+  Neighbor(const Neighbor<T>& copy)
+      : m_id(copy.m_id), m_cost(copy.m_cost)
+  {}
+
+};
+
+template <typename T>
+ostream& operator<<(ostream& out, const Neighbor<T>& n)
+{
+  out << "\"" << n.m_id << "\" (cost $" << n.m_cost << ")";
+
+  return out;
+}
+
+template <typename T>
+istream& operator>>(istream& in, Neighbor<T>& n)
+{
+  in >> n.m_id >> n.m_cost;
+
+  return in;
+}
+
+#endif //ZDAY_NEIGHBOR_H
