ca60b1e3222d39dc9dc5471a7018dbd49d91a760
Grant Broadwater <broadwater.grant@gmail.com>
2016-04-26 18:24:56 -0500

    Write main function to get graph input and output the graph.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
+//
+// Created by Grant Broadwater on 4/26/16.
+//
+
 #include <iostream>
+
+#include "graph.h"
 
 using namespace std;
 
-int main() {
-  cout << "Hello, World!" << endl;
+int main()
+{
+  short numTestCases;
+  short bendersTokens;
+  string cityName;
+  Graph<string> cityMap;
+
+  cin >> numTestCases;
+
+  for(int i=0; i<numTestCases; i++)
+  {
+    cin >> bendersTokens >> cityName >> cityMap;
+
+    cout << "City: " << cityName << endl;
+    cout << "Tokens: " << bendersTokens << endl;
+    cout << "Graph: " << endl << cityMap << endl;
+  }
+
   return 0;
 }
